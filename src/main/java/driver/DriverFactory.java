2cb1997e6865d813947493d6da048d8191f58fa9
periyasamymuthuqa-prog <periyasamymuthu.qa@gmail.com>
2026-07-17 18:07:08 +0530

    Create DriverFactory.java

---
+++ src/main/java/driver/DriverFactory.java
@@ -0,0 +1,38 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+
+
+    private static WebDriver driver;
+
+
+    public static void initializeDriver() {
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+    }
+
+
+    public static WebDriver getDriver() {
+
+        return driver;
+
+    }
+
+
+    public static void quitDriver() {
+
+        if(driver != null) {
+
+            driver.quit();
+
+        }
+
+    }
+
+}
