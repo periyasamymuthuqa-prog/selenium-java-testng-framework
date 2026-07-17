4e7c5b9e120b052bf5efa055398bc4246845c34a
periyasamymuthuqa-prog <periyasamymuthu.qa@gmail.com>
2026-07-17 18:12:32 +0530

    Update BaseTest.java

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,28 +1,55 @@
 package base;
 
 
+import org.testng.ITestResult;
+
 import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.BeforeMethod;
 
+
 import driver.DriverFactory;
+
+import utils.ScreenshotUtil;
+
 
 
 public class BaseTest {
 
 
-    @BeforeMethod
-    public void setUp() {
 
-        DriverFactory.initializeDriver();
+@BeforeMethod
+
+public void setUp(){
+
+    DriverFactory.initializeDriver();
+
+}
+
+
+
+@AfterMethod
+
+public void tearDown(
+        ITestResult result){
+
+
+    if(result.getStatus()
+            == ITestResult.FAILURE){
+
+
+        ScreenshotUtil.captureScreenshot(
+        DriverFactory.getDriver(),
+        result.getName());
+
 
     }
 
 
-    @AfterMethod
-    public void tearDown() {
+    DriverFactory.quitDriver();
 
-        DriverFactory.quitDriver();
 
-    }
+}
+
 
 }
